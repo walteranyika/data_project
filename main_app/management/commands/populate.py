0616966394edad147d5cbf709682753b499aa3ea
Walter Indeche <walter.indeche@andela.com>
2023-11-08 11:22:58 +0300

    Added progress to processing

--- main_app/management/commands/populate.py
+++ main_app/management/commands/populate.py
@@ -2,6 +2,7 @@ import json
 import os.path
 
 from django.core.management import BaseCommand
+from tqdm import trange
 
 from data_project import settings
 from main_app.models import Employee
@@ -20,9 +21,14 @@ class Command(BaseCommand):
             self.style.SUCCESS('Started populating the db')
         )
 
+        # pip install tqdm
+        progress = trange(200)
+        index = 0
         with open(os.path.join(settings.BASE_DIR, "data.json")) as file:
             data = json.load(file)
             for record in data:
+                index += 1
+                progress.set_description("Processed %s / 200" % str(index))
                 Employee.objects.create(
                     name=record["name"], email=record["email"], dob=record["dob"], salary=round(record["salary"]), disabled=record["disabled"])
 
