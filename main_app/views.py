c0cfdb4dea5d78ea8ad138d1e26b7163bb2e2c94
Walter Indeche <walter.indeche@andela.com>
2023-11-08 16:57:10 +0300

    Add Query filters

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
 
 from main_app.app_forms import EmployeeForm
@@ -22,7 +25,17 @@ def home(request):
 # All employees
 # One employee
 def all_employees(request):
-    employees = Employee.objects.all()  # SELECT * FROM employees
+    # employees = Employee.objects.all()  # SELECT * FROM employees
+    # employees = Employee.objects.all().order_by("-salary")
+    # employees = Employee.objects.filter(name__istartswith="La").order_by("dob")
+    # employees = Employee.objects.filter(name__istartswith="La", salary__gt=45000).order_by("dob")
+    # employees = Employee.objects.filter(Q(name__contains="la") | Q(salary__gt=70000))
+    # employees = Employee.objects.filter(Q(name__contains="la") & Q(salary__gt=70000))
+    # employees = Employee.objects.filter(Q(name__contains="la") & ~Q(salary__gt=70000)) # tilde
+    today = datetime.today()
+    day = today.day
+    month = today.month
+    employees = Employee.objects.filter(dob__day=day, dob__month=month)  # tilde
     return render(request, "all_employees.html", {"employees": employees})
 
 
@@ -31,6 +44,7 @@ def employee_details(request, emp_id):
     return render(request, "employee_details.html", {"employee": employee})
 
 
+# employees/delete/12000
 def employee_delete(request, emp_id):
     employee = get_object_or_404(Employee, pk=emp_id)
     employee.delete()
