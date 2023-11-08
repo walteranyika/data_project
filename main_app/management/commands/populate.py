a0ed73230c0e8504a866015ca1e01cf37a342696
Walter Indeche <walter.indeche@andela.com>
2023-11-08 15:49:15 +0300

    Complete the handle function

--- main_app/management/commands/populate.py
+++ main_app/management/commands/populate.py
@@ -1,8 +1,31 @@
+import json
+import os.path
+
 from django.core.management import BaseCommand
+
+from data_project import settings
+from main_app.models import Employee
 
 
 class Command(BaseCommand):
     help = "Populates employees table with 1000 records"
 
     def handle(self, *args, **options):
-        pass
+        path = os.path.join(settings.BASE_DIR, "employees.json")
+        self.stdout.write(
+           self.style.SUCCESS("Started to import data")
+        )
+        with open(path) as file:  # file = open(path)
+            employees = json.load(file)
+            for e in employees:
+                Employee.objects.create(
+                    name=e['name'],
+                    email=e['email'],
+                    dob=e['dob'],
+                    salary=e['salary'],
+                    disabled=e["disabled"],
+                )
+
+        self.stdout.write(
+            self.style.SUCCESS("Completed  importing data")
+        )
